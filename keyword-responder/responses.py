1593ee2be0555b1c862cfb04789b9770f6f3f892
Cheston Ovington <167095109+covingp6@users.noreply.github.com>
2025-08-05 22:49:30 -0400

    Update responses.py
    Updated intent and responses to account for time and define functions

--- keyword-responder/responses.py
+++ keyword-responder/responses.py
@@ -3,13 +3,17 @@ intent_map = {
     "farewell": ["bye", "see you", "bye"],
     "thanks": ["thanks", "thank you"],
     "weather": ["weather"],
+    "time": ["time"],
+    "define": ["define", "definition"]
 }
 
 responses = {
     "greeting": ["Hi!", "Hello!", "Nice to meet you!"],
     "farewell": ["Bye!", "Take care!", "See you soon!"],
     "thanks": ["You're welcome!", "No worries!", "Glad I could help!"],
-    "weather": ["What location would you like the weather for?"]
+    "weather": ["Got it. Which location should I check?", "Sure! Just let me know where to check.", "Which location are you curious about the weather in?"],
+    "time": ["I'm on it, just tell me which location you're interested in.", "Just give me a location and I'm on it.", "Which location should I check the current time for?"],
+    "define": ["Sure, what word do you need the definition of?", "Okay, what word do you need me to define?", "Alright, what word should I define?"]
 }
 
 def get_intent(user_input):
